94e0e5fd385aaf0212ea203f0b85ce5e09ccde13
DePelows <alarconmarcelo383@gmail.com>
2026-09-18 21:48:50 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = "upch-notas-v2";
+
+// Rutas actualizadas con la estructura de carpetas
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./css/styles.css",
+  "./js/app.js",
+  "./manifest.json",
+  "./assets/icon-192.png",
+  "./assets/icon-512.png"
+];
+
+// Instalación
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Interceptar peticiones para el offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
